d1bfe935394a225cb64b9d31635bfcdc9ce463cf
Johanny Amaya <jamayau@unal.edu.co>
2022-08-28 16:10:10 -0500

    lista de las funcionalidades de la app completas

--- API country languaje.py
+++ API country languaje.py
@@ -1,8 +1,25 @@
 import pandas as pd
 
+## Consumir API
+
+
+
+## Calcular tiempos
+
+
+
 
 ## Create dataframe
 
 df_vacio=pd.DataFrame(columns=["Region", "City Name", "Language", "Time"])
 
 print(df_vacio)
+
+## Calcular iempo total, el tiempo promedio, el tiempo mínimo y el máximo que tardo
+
+
+
+## Guardar en sqlite
+
+
+## Generar  Json con la tabla y guardar en data.json
