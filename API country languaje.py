832ea915d6c40037154e3d6bd22449ca97c88aa8
Johanny Amaya <jamayau@unal.edu.co>
2022-08-29 07:31:23 -0500

    avance

--- API country languaje.py
+++ API country languaje.py
@@ -1,6 +1,14 @@
 import hashlib
 import pandas as pd
 
+
+
+# Convertir info en dicccionario
+def country_info(region,city_name,lang_sha1_hex, time):
+    h={'Region': region, 'City Name': city_name, 'Language': lang_sha1_hex,'Time': time}
+    return h
+
+
 ## Obtener respuesta de https://restcountries.com/ idioma del pais
 
 languaje= "portuguese"
@@ -10,7 +18,6 @@ lang=languaje.encode()
 lang_sha1 = hashlib.sha1(lang)
 lang_sha1_hex = lang_sha1.hexdigest() # Convierte a formato hexadecimal
 print(lang_sha1_hex)
-
 ## Calcular tiempos
 
 
@@ -23,17 +30,19 @@ df=pd.DataFrame(columns=["Region", "City Name", "Language", "Time"])
 print(df)
 # Llenar el Dataframe
 
-df_new=pd.DataFrame({'Region': [2, 4], 'City Name': [2, 0], 'Language': [2, 0],'Time': [2, 0]}, index=['falcon', 'dog'])
+df_new=pd.DataFrame({'Region': [2, 4], 'City Name': [2, 0], 'Language': lang_sha1_hex,'Time': [2, 0]}, index=['falcon', 'dog'])
 
 
 
 df_new2=pd.DataFrame({'Region': [56,3], 'City Name': [26,98], 'Language': [322, 50],'Time': [32, 40]},index=['falcata', 'doggy'])
 
+df_new3=pd.DataFrame(country_info("Egipto","El Cairo","gerf563t343",0.345),index=['falcata', 'doggy'])
+
 df=pd.concat([df,df_new],sort=False)
 
 df=pd.concat([df,df_new2],sort=False)
 
-#df=df.append(df_new,ignore_index=True)
+df=pd.concat([df,df_new3],ignore_index=True)
 
 print(df)
 ## Calcular tiempo total, el tiempo promedio, el tiempo mínimo y el máximo que tardo
