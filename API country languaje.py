46fa79798cac633ffd45ac2f2aeb41bfa253382a
Johanny Amaya <jamayau@unal.edu.co>
2022-08-29 00:19:30 -0500

    primeros modulos

--- API country languaje.py
+++ API country languaje.py
@@ -1,8 +1,15 @@
+import hashlib
 import pandas as pd
 
-## Consumir API
+## Obtener respuesta de https://restcountries.com/ idioma del pais
 
+languaje= "portuguese"
+lang=languaje.encode()
 
+## Tomar el idioma y encriptarlo en SHA1
+lang_sha1 = hashlib.sha1(lang)
+lang_sha1_hex = lang_sha1.hexdigest() # Convierte a formato hexadecimal
+print(lang_sha1_hex)
 
 ## Calcular tiempos
 
@@ -11,15 +18,30 @@ import pandas as pd
 
 ## Create dataframe
 
-df_vacio=pd.DataFrame(columns=["Region", "City Name", "Language", "Time"])
+df=pd.DataFrame(columns=["Region", "City Name", "Language", "Time"])
 
-print(df_vacio)
+print(df)
+# Llenar el Dataframe
 
-## Calcular iempo total, el tiempo promedio, el tiempo mínimo y el máximo que tardo
+df_new=pd.DataFrame({'Region': [2, 4], 'City Name': [2, 0], 'Language': [2, 0],'Time': [2, 0]}, index=['falcon', 'dog'])
+
+
+
+df_new2=pd.DataFrame({'Region': [56,3], 'City Name': [26,98], 'Language': [322, 50],'Time': [32, 40]},index=['falcata', 'doggy'])
+
+df=pd.concat([df,df_new],sort=False)
+
+df=pd.concat([df,df_new2],sort=False)
+
+#df=df.append(df_new,ignore_index=True)
+
+print(df)
+## Calcular tiempo total, el tiempo promedio, el tiempo mínimo y el máximo que tardo
 
 
 
 ## Guardar en sqlite
+
 
 
 ## Generar  Json con la tabla y guardar en data.json
